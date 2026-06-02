2c6ece21047074c7b0fd11f869821718da3364ec
Deepesh Jangid <deepeshjangid269@gmail.com>
2026-06-02 11:49:07 +0530

    Question 1

---
+++ ObjectandConstructor.java
@@ -0,0 +1,30 @@
+//package Assignment;
+//
+//class Employee{
+//	int Employee_ID;
+//	String Employee_Name;
+//	float Salary;
+//	
+//	public Employee(int Employee_ID, String Employee_Name,float Salary) {
+//		this.Employee_ID = Employee_ID;
+//		this.Employee_Name = Employee_Name;
+//		this.Salary = Salary;
+//	}
+//	
+//	void display() {
+//		System.out.println("Employee_id: " + Employee_ID);
+//		System.out.println("Employee_Name: " + Employee_Name);
+//		System.out.println("Employee_Salary: " + Salary);
+//	}
+//}
+//
+//
+//
+//public class ObjectandConstructor {
+//
+//	public static void main(String[] args) {
+//		Employee E = new Employee(1, "Gajverdhan",20000);
+//		E.display();
+//	}
+//
+//}
